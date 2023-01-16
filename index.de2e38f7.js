65085bf1c0b9bb59a67ab4048e5b6631374cbfdc
333Nikita333 <110134863+333Nikita333@users.noreply.github.com>
2023-01-16 00:24:59 +0000

    Deploying to gh-pages from @ 333Nikita333/goit-js-hw-11@6dd6b3d2e631be120b19bfd216600ee5fdfdb89c 🚀

--- index.de2e38f7.js
+++
@@ -1,2 +0,0 @@
-!function(){var e=document.querySelector(".search-bar");e.addEventListener("input",(function(){console.log(e.elements.searchQuery.value)}))}();
-//# sourceMappingURL=index.de2e38f7.js.map
